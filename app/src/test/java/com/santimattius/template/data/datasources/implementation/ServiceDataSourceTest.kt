251c7fc77fbbfd1225a956f0ddafeba9d37e2b3b
Santiago Mattiauda <santimattius@gmail.com>
2022-03-13 11:50:27 -0300

    [feature] improvement dependencies + test fixs

--- app/src/test/java/com/santimattius/template/data/datasources/implementation/ServiceDataSourceTest.kt
+++ app/src/test/java/com/santimattius/template/data/datasources/implementation/ServiceDataSourceTest.kt
@@ -1,9 +1,10 @@
 package com.santimattius.template.data.datasources.implementation
 
-import com.santimattius.template.objectmothers.NetworkPictureMother
 import com.santimattius.template.core.data.Result
 import com.santimattius.template.data.datasources.implementation.client.PicSumClient
 import com.santimattius.template.data.datasources.implementation.client.ServiceError
+import com.santimattius.template.objectmothers.NetworkPictureMother
+import com.santimattius.template.utils.CoroutinesTestRule
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -11,36 +12,43 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual
+import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class ServiceDataSourceTest {
 
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
     private val picSumClient: PicSumClient = mockk()
     private val serviceDataSource: ServiceDataSource = ServiceDataSource(picSumClient)
 
     @Test
-    fun `get picture on client result is success`() = runBlockingTest {
-
+    fun `get picture on client result is success`() {
         // Given
         val pictures = NetworkPictureMother.pictures()
         coEvery { picSumClient.fetchList() } returns Result.success(pictures)
         // When
-        val result = serviceDataSource.getPictures()
-        // Then
-        assertThat(result, IsEqual(pictures))
+        runBlockingTest {
+            val result = serviceDataSource.getPictures()
+            // Then
+            assertThat(result, IsEqual(pictures))
+        }
         coVerify { picSumClient.fetchList() }
     }
 
     @Test
-    fun `get picture on client result is fail`() = runBlockingTest {
+    fun `get picture on client result is fail`() {
         // Given
         val failure = Result.failure(ServiceError("test case on fail"))
         coEvery { picSumClient.fetchList() } returns failure
         // When
-        val result = serviceDataSource.getPictures()
-        // Then
-        assertThat(result, IsEqual(emptyList()))
+        runBlockingTest {
+            val result = serviceDataSource.getPictures()
+            // Then
+            assertThat(result, IsEqual(emptyList()))
+        }
         coVerify { picSumClient.fetchList() }
     }
 }
--- app/src/test/java/com/santimattius/template/domain/usecases/GetPicturesTest.kt
+++ app/src/test/java/com/santimattius/template/domain/usecases/GetPicturesTest.kt
@@ -1,6 +1,7 @@
 package com.santimattius.template.domain.usecases
 
 import com.santimattius.template.domain.repositories.PicturesRepository
+import com.santimattius.template.utils.CoroutinesTestRule
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -8,22 +9,27 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual
+import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class GetPicturesTest {
 
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
     private val repository: PicturesRepository = mockk()
     private val useCase: GetPictures = GetPictures(repository)
 
     @Test
-    fun `invoke get picture use case`() = runBlockingTest {
+    fun `invoke get picture use case`() {
         // Given
         coEvery { repository.getPictures() } returns emptyList()
-        // When
-        val result = useCase.invoke()
-        // Then
-        assertThat(result, IsEqual(emptyList()))
+        runBlockingTest {// When
+            val result = useCase.invoke()
+            // Then
+            assertThat(result, IsEqual(emptyList()))
+        }
         coVerify { repository.getPictures() }
     }
 }
