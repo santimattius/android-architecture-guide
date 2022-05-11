9fd372dbad729ce1e71d8e286a9b7cc10f1b0af8
Santiago Mattiauda <santimattius@gmail.com>
2022-05-11 18:55:15 -0300

    [feature] koin 3.2

---
+++ app/src/test/java/com/santimattius/template/di/AppConfigurationTest.kt
@@ -0,0 +1,25 @@
+package com.santimattius.template.di
+
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.koinApplication
+import org.koin.test.KoinTest
+import org.koin.test.check.checkModules
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(manifest = Config.NONE, sdk = [Build.VERSION_CODES.R])
+class AppConfigurationTest : KoinTest {
+    @Test
+    fun verifyKoinApp() {
+        koinApplication {
+            androidContext(ApplicationProvider.getApplicationContext())
+            modules(modulesDefinitions)
+            checkModules()
+        }
+    }
+}
